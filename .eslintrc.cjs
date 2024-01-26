777f07cd58baf8d1cbf373d9b53e324c51a018c1
Wujue <wujue0115@gmail.com>
2024-01-26 10:52:47 +0800

    chore: add eslint and prettier with airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,39 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
+    'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+      },
+    ],
+    'max-len': [
+      'error',
+      {
+        ignorePattern: 'd="([\\s\\S]*?)"',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
+  },
+};
