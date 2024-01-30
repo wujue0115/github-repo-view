1c11e09159c105d44b27826b20f401d36d3ed03d
Wujue <wujue0115@gmail.com>
2024-01-30 15:34:02 +0800

    chore: install unplugin-auto-import and unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,27 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/github-repo-view/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({ dts: true }),
+    AutoImport({
+      include: [
+        /\.[tj]s$/, // .ts, .js,
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue', 'vue-router'],
+      dirs: ['./composables', './stores'],
+      dts: './auto-imports.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
