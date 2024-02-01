40de77132ad7f6dd7e8726d61cd51c2c23946350
Wujue <wujue0115@gmail.com>
2024-02-01 14:12:51 +0800

    feat: add head information

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { useHead } from '@unhead/vue';
+
+useHead({
+  title: 'GitHub Repo View',
+  meta: [{ name: 'description', content: "Show GitHub user's repositories" }],
+});
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import './assets/base.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createHead } from '@unhead/vue';
 
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
+const head = createHead();
 
 app.use(createPinia());
 app.use(router);
+app.use(head);
 
 app.mount('#app');
